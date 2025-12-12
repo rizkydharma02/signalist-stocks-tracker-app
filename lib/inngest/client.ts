ba0291ea01ca276d073f829d2f72d338ea7c0a86
rizkydharma02 <rizkydharma02@gmail.com>
2025-12-12 14:48:38 +0700

    add inngest eventkey and signingkey

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,6 +2,9 @@ import { Inngest } from 'inngest';
 
 export const inngest = new Inngest({
   id: 'Signalist',
+  name: 'Signalist',
+  eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   ai: {
     gemini: {
       apiKey: process.env.GEMINI_API_KEY,
